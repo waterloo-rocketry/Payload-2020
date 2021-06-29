17fa5971635679fe2272b258d5ee2e30f80062e0
Christopher Chung <tps90067@gmail.com>
2021-06-28 20:40:12 -0400

    work on pin init and led init

--- mama_board_code.X/led.c
+++ mama_board_code.X/led.c
@@ -7,6 +7,14 @@
 void led_init()
 {
     
+    \//CHANGE PINS TO CORRECT
+    TRISBbits.TRISB4 = 0; // Set output
+    LATBbits.LATB4 = 0; //LED off
     
+    TRISBbits.TRISB3 = 0; // Set output
+    LATBbits.LATB3 = 0; //LED off
+    
+    TRISBbits.TRISB2 = 0; //Set output
+    LATBbits.LATB3 = 0; //LED off
     
 }
--- mama_board_code.X/pin_interrupt.c
+++ mama_board_code.X/pin_interrupt.c
@@ -2,12 +2,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pic18f26k83.h>
 
 void pin_init()
 {
+    PIE0bits.IOCIE = 1; //Set bit to generate interrupts on change
     
-    
-    
+    // Input pins CHANGE TO CORRECT
+    TRISAbits.TRISA0 = 1;
+    TRISAbits.TRISA1 = 1;
+    TRISAbits.TRISA2 = 1;
+     
+    // Interrupt on rising edge
+    IOCAPbits.IOCAP0 = 1;
+    IOCAPbits.IOCAP1 = 1;
+    IOCAPbits.IOCAP2 = 1;
 }
 
 void pin_interrupt_handler()
